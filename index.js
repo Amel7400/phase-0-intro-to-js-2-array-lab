359be1f473bbeb3d116ee8a1df857ee53d7f4923
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-08-04 01:45:22 +0200

    Arrays Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(name){
+  cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat(){
+  cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift("Milo")
+}
+function appendCat(name) {
+  const newCatsArray = [...cats, "Broom"];
+  return newCatsArray;
+}
+function prependCat(name){
+  const newCatArray = ["Arnold", ...cats];
+  return newCatArray;
+}
+function removeLastCat(name){
+  const newCatArray = cats.slice(0, cats.length - 1);
+  return newCatArray;
+}
+function removeFirstCat(name){
+  const newCatArray = cats.slice(1);
+  return newCatArray;
+}
